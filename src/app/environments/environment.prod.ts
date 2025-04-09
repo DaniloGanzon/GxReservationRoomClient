5a76a27270c255d7b495a91282ccb1ddfef5c468
Aki Ganzon <akiganzon02@gmail.com>
2025-04-09 17:27:50 +0800

    Fixed environments11

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://bookroomgx-api.onrender.com' 
-  };
+  production: true,
+  apiUrl: 'https://bookroomgx-api.onrender.com/api'
+};
